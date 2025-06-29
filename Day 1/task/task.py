3475a63a9ed283f14ef5199db18ba223a1bc1fc9
Dasha Akhten <dashaakhten@MacBookPro.lan>
2025-06-28 19:18:40 -0700

    feat: band name generator

--- Day 1/task/task.py
+++ Day 1/task/task.py
@@ -6,6 +6,8 @@ print ("this is probs not the band name you want" + "!")
 # print(input("What was the name of your first pet?")
 # )
 city_name = input("What city did you grow up in?")
+print(city_name)
 pet_name = input("What was the name of your first pet?")
+print (pet_name)
 
 print ("Your band name (or password hint) is " + city_name  +" " + pet_name)
