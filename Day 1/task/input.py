8fff6c299f3707bb8eca8671d408f07db672eefe
Dasha Akhten <dashaakhten@MacBookPro.lan>
2025-06-28 19:16:42 -0700

    band name

--- Day 1/task/input.py
+++
@@ -1,8 +0,0 @@
-input("What is your name")
-
-#this is an input, we give the user something to do
-
-print("Hello " + input("What is your name"))
-
-print("Hello "+ input("What is your name") + "!")
-
--- Day 1/task/task.py
+++ Day 1/task/task.py
@@ -1,8 +1,11 @@
-# Write your code below this line 👇
+print ("this is probs not the band name you want" + "!")
 
-print("hello world\nRUN\nthis is a string")
+# print(input("What city did you grow up in?")
+# )
+#
+# print(input("What was the name of your first pet?")
+# )
+city_name = input("What city did you grow up in?")
+pet_name = input("What was the name of your first pet?")
 
-print("this string" + " " + "is concatenated")
-
-
-
+print ("Your band name (or password hint) is " + city_name  +" " + pet_name)
